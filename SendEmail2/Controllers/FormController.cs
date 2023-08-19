cad26c77db70e43017c0aa39a03e16e4089c12be
Ahmad Bayrakdar <ahmad.berkdar.sy@gmail.com>
2023-08-19 10:23:23 +0400

    Add project files.

---
+++ SendEmail2/Controllers/FormController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Common.Filters;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace SendEmail2.Controllers
+{
+    public class FormController : SurfaceController
+    {
+        public FormController(
+            IUmbracoContextAccessor umbracoContextAccessor,
+            IUmbracoDatabaseFactory databaseFactory,
+            ServiceContext services,
+            AppCaches appCaches,
+            IProfilingLogger profilingLogger,
+            IPublishedUrlProvider publishedUrlProvider)
+            : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        [HttpPost]
+        [ValidateUmbracoFormRouteString]
+        public ActionResult Submit()
+        {
+            try
+            {
+                // Your action logic
+                return Content("hi");
+            }
+            catch (Exception ex)
+            {
+                return Content("Error in Submit action");
+                throw; // Rethrow the exception to let it propagate
+            }
+        }
+    }
+}
+
+
